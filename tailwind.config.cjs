4d513d96ef096e1f25a3788b1fbfab082994e326
tawhidjony <tawhid.jony.jhm@gmail.com>
2022-09-22 12:36:47 +0600

    aboute page issue fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Quicksand", sans-serif;', ...defaultTheme.fontFamily.sans],
+        'sans': ['"Quicksand", sans-serif', ...defaultTheme.fontFamily.sans],
       },
     },
   },
