8568c8166bdc4fc6cced4bb32e0e1bcea69c8de8
Tawhid Jony <tawhid.jony.jhm@gmail.com>
2025-07-03 15:37:21 +0600

    about

--- app/about/AboutPage.tsx
+++ app/about/AboutPage.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router";
 
 export function AboutPage() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      about
+      about of new routes
+      <Link to={"/"} >Home</Link>
     </main>
   );
 }
